a86b23e76fb391e5bd7c473361e3579be822b9dc
Sunny Liu <sunnyliu2@gmail.com>
2025-07-16 07:14:48 -0400

    Update add Table Column, PrimaryKey Foreignkey to __init__.py

--- pgsql_parser/__init__.py
+++ pgsql_parser/__init__.py
@@ -1,3 +1,3 @@
-from .sqlparser import SQLLexer, SQLParser, TokenType, Token
+from .sqlparser import SQLLexer, SQLParser, TokenType, Token, Table, Column, PrimaryKey, ForeignKey
 
-__all__ = ["SQLLexer", "SQLParser", "TokenType", "Token"]
+__all__ = ["SQLLexer", "SQLParser", "TokenType", "Token", "Table", "Column", "PrimaryKey", "ForeignKey"]
